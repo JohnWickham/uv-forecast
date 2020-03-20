d291944638dd8cb46d72b9f25d90fd22e69f58a1
JohnWickham <john@wjwickham.com>
2020-03-20 12:27:03 -0400

    Initial Commit

---
+++ UV Forecast WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UV Forecast WatchKit Extension
+//
+//  Created by John Wickham on 3/20/20.
+//  Copyright © 2020 Wickham. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UV Forecast WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  UV Forecast WatchKit Extension
+//
+//  Created by John Wickham on 3/20/20.
+//  Copyright © 2020 Wickham. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
