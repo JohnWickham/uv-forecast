d23913408f97ca8fe3aeb61d83f54ec73e0cba9b
JohnWickham <john@wjwickham.com>
2020-06-26 13:12:54 -0400

    Update mock data

--- UV Forecast WatchKit Extension/Data Model/MockData.swift
+++ UV Forecast WatchKit Extension/Data Model/MockData.swift
@@ -21,9 +21,14 @@ let timelineEntries: [ForecastTimelineEntry] = [
 	UVForecast(date: Date(timeIntervalSince1970: 1586386800), uvIndex: UVIndex(uvValue: 0.0)),
 	Night(date: Date(timeIntervalSince1970: 1586303400), endDate: Date(timeIntervalSince1970: 1586343600))
 ]
-let days: [Day] = [
-	Day(startDate: Date(), sunriseDate: Date(timeIntervalSince1970: 1586257320), sunsetDate: Date(timeIntervalSince1970: 1586303400), daytimeForecasts: timelineEntries, allForecasts: timelineEntries, highForecast: timelineEntries.max() as! UVForecast)
-]
+
+var days: [Day] {
+	var days: [Day] = []
+	for i in 0..<8 {
+		days.append(Day(startDate: Date() + TimeInterval(86400 * i), sunriseDate: Date(timeIntervalSince1970: 1586257320), sunsetDate: Date(timeIntervalSince1970: 1586303400), daytimeForecasts: timelineEntries, allForecasts: timelineEntries, highForecast: timelineEntries.max() as! UVForecast))
+	}
+	return days
+}
 
 let mockDataTodayTimeline = ForecastTimeline(days: days)
 let mockDataForecastTimeline = ForecastTimeline(days: days)
